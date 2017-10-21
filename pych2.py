5ea293420712b5f9c5656c3ac77668c1f138808e
Adam <adambens@umich.edu>
2017-10-20 20:17:59 -0400

    Uploading final version tested on updated directory

---
+++ pych2.py
@@ -0,0 +1,13 @@
+import re
+
+#level one Python Challenge
+
+s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+"""
+Add 2 to existing ascii values.
+
+if value is 121 (y) or 122 (z)
+change to a,b respectively
+
+"""
